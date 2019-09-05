038482eb10415ab7e386b814792bcba589c03ce9
tousifahammad <tousif115125@gmailcom>
2019-09-05 15:21:47 +0530

    input field
    how to use input field and one way data binding

--- Project/react-complete-guide/src/App.js
+++ Project/react-complete-guide/src/App.js
@@ -4,47 +4,77 @@ import Person from './Person/Person.js'
 
 class App extends Component {
 
-  state = {
-    persons: [
-      { name: "Tousif", age: "26" },
-      { name: "Shantanu da", age: "31" },
-      { name: "Shohel", age: "25" }
-    ]
-  }
-
-  swithNameHandler = () =>{
-    this.setState(
-      {
+    state = {
         persons: [
-          { name: "Tousif Ahammad", age: "27" },
-          { name: "Shantanu Mondal", age: "37" },
-          { name: "Shohel Akhtar", age: "27" }
+            { name: "Tousif", age: "26" },
+            { name: "Shantanu da", age: "31" },
+            { name: "Shohel", age: "25" }
         ]
-      }
-    )
-  }
+    }
 
-  render() {
-    return (
-      <div className="App">
-        <h1>Hello Tousif. Welcome to React</h1>
+    swithNameHandler = () => {
+        this.setState(
+            {
+                persons: [
+                    { name: "Tousif Ahammad", age: "27" },
+                    { name: "Shantanu Mondal", age: "37" },
+                    { name: "Shohel Akhtar", age: "27" }
+                ]
+            }
+        )
+    }
 
-        <h2>Using props</h2>
-        <Person name="Tousif" age="26"> I am a developer </Person>
-        <Person name="Shantanu da" age="31" />
-        <Person name="Shohel" age="25" />
+    autoNameChangeHandler = (event) => {
+        this.setState(
+            {
+                persons: [
+                    { name: event.target.value, age: "27" },
+                    { name: event.target.value, age: "37" },
+                    { name: event.target.value, age: "27" }
+                ]
+            }
+        )
+    }
 
-        <h2>Using props with state</h2>
+    render() {
+        return (
+            <div className="App">
+                <h1>Hello Tousif. Welcome to React</h1>
 
-        <button onClick = {this.swithNameHandler}> Switch Name </button>
+                {/* <h2>Using props</h2>
+                <Person name="Tousif" age="26"> I am a developer </Person>
+                <Person name="Shantanu da" age="31" />
+                <Person name="Shohel" age="25" /> */}
 
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}> I am a developer </Person>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+                <h2>Using props with state</h2>
 
-      </div>
-    );
-  }
+                <button onClick={this.swithNameHandler}> Switch Name </button>
+
+                <Person
+                    name={this.state.persons[0].name}
+                    age={this.state.persons[0].age}
+                    change_name={this.swithNameHandler}
+                    on_change={this.autoNameChangeHandler}>
+                    I am a developer
+                </Person>
+
+                <Person
+                    name={this.state.persons[1].name}
+                    age={this.state.persons[1].age}
+                    change_name={this.swithNameHandler}
+                    on_change={this.autoNameChangeHandler}>
+                </Person> />
+
+                <Person
+                    name={this.state.persons[2].name}
+                    age={this.state.persons[2].age}
+                    change_name={this.swithNameHandler}
+                    on_change={this.autoNameChangeHandler}>
+                </Person>/>
+
+            </div>
+        );
+    }
 }
 
 export default App;
--- Project/react-complete-guide/src/Person/Person.js
+++ Project/react-complete-guide/src/Person/Person.js
@@ -1,13 +1,15 @@
 import React from 'react';
 
 const person = (props) => {
-    return(
+    return (
         <div>
             <h4> {props.name} : {props.age}</h4>
             <p> {props.children}</p>
             <p> The journey start before  {props.age} year </p>
-            <br/>
-        </div> 
+            <button onClick = {props.change_name}> Change name</button>
+            <input type = "text" onChange = {props.on_change}></input>
+            <br />
+        </div>
     )
 }
 
