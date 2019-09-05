7e1fc252da6adb3c3b14a24555167cada5a651d9
tousifahammad <tousif115125@gmailcom>
2019-09-05 12:21:35 +0530

    how to state

--- Project/react-complete-guide/src/App.js
+++ Project/react-complete-guide/src/App.js
@@ -3,13 +3,30 @@ import './App.css';
 import Person from './Person/Person.js'
 
 class App extends Component {
+
+  state = {
+    persons: [
+      { name: "Tousif", age: "26" },
+      { name: "Shantanu da", age: "31" },
+      { name: "Shohel", age: "25" }
+    ]
+  }
+
   render() {
     return (
       <div className="App">
-          <h1>Hello Tousif. Welcome to React</h1>
-          <Person name = "Tousif" age= "26"> I am a developer </Person>
-          <Person name = "Shantanu da" age= "31"/>
-          <Person name = "Shohel" age= "25"/>
+        <h1>Hello Tousif. Welcome to React</h1>
+
+        <h2>Using props</h2>
+        <Person name="Tousif" age="26"> I am a developer </Person>
+        <Person name="Shantanu da" age="31" />
+        <Person name="Shohel" age="25" />
+
+        <h2>Using props with state</h2>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}> I am a developer </Person>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+
       </div>
     );
   }
--- Project/react-complete-guide/src/Person/Person.js
+++ Project/react-complete-guide/src/Person/Person.js
@@ -3,10 +3,10 @@ import React from 'react';
 const person = (props) => {
     return(
         <div>
-            <h1> {props.name} : {props.age}</h1>
-            <h1> {props.children}</h1>
-            <p> The journey start before  {Math.random()} year </p>
-
+            <h4> {props.name} : {props.age}</h4>
+            <p> {props.children}</p>
+            <p> The journey start before  {props.age} year </p>
+            <br/>
         </div> 
     )
 }
